3270127968c369562aea829fb7df0981000e083f
lauuurars <1120098177@u.icesi.edu.co>
2025-04-04 00:21:16 -0500

    fin punto 10

---
+++ Act. 10 Casa/ciclos10.js
@@ -0,0 +1,9 @@
+//Desarrollo Actividad 10
+
+const nombres = ["Santiago", "Manuela", "Luisa", "Andres", "Thomas"] //creamos array con const
+
+for (const nombre of nombres) {
+    console.log(nombre.toUpperCase()) //convertimos nombres a mayusculas
+}
+
+//for of recorre los valores del array (en este caso, cada nombre).
