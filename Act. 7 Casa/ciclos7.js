c4afe3d8972b2a4e554b996fde7ce435bd6e38d4
lauuurars <1120098177@u.icesi.edu.co>
2025-04-04 00:03:08 -0500

    fin punto 7

---
+++ Act. 7 Casa/ciclos7.js
@@ -0,0 +1,15 @@
+//Desarrollo Actividad 7
+
+const frutas = ["fresa", "sandia", "pera", "mango", "piña"] //creamos array usando const ya que no se reemplazarán datos
+
+for (let i = 0; i < frutas.length; i++) { 
+    console.log((i + 1) + ". " + frutas[i]); 
+}
+
+//let i = 0; permite iniciar desde la primera fruta
+
+//i < frutas.length; permite que el ciclo se ejecute mientras i sea menor que la longitud del array frutas
+
+//i++ permite incrementar el valor de i en 1 cada vez que se ejecute el ciclo
+
+//console.log((i + 1) + ". " + frutas[i]); permite mostrar el número de la fruta y su nombre en la consola.
