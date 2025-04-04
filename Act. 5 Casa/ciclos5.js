2a718cd52661e5a3b5cc17849cfd36f51b1c5f67
lauuurars <1120098177@u.icesi.edu.co>
2025-04-04 15:00:42 -0500

    se arregñó código actividad 5

--- Act. 5 Casa/ciclos5.js
+++ Act. 5 Casa/ciclos5.js
@@ -1,18 +1,17 @@
-//Desarrolo Actividad 5
 
-const adivinaNumero = Math.floor(Math.random() * 50) + 1; //generamos un número aleatorio entre 1 y 50
+//Desarrollo Actividad 5
 
-let intento; //variable para guardar info sobre el intento del usuario
+let numeroSecreto = Math.floor(Math.random() * 50) + 1; //Generar un número aleatorio entre 1 y 50
 
-do {
-    intento = parseInt(prompt("Adivina el número entre 1 y 50:"));
+let intento = parseInt(prompt("Adivina el número entre 1 y 50:")); //solicitamos un número al usuario
 
-    if (intento > adivinaNumero) {
-        console.log("Demasiado alto, intenta de nuevo.");
-    } else if (intento < adivinaNumero) {
-        console.log("Demasiado bajo, intenta de nuevo.");
+
+do  {
+    if (intento > numeroSecreto) {
+        intento = parseInt(prompt("El número es menor. Inténtalo de nuevo:"));
     } else {
-        console.log("¡Felicidades, adivinaste el número!");
+        intento = parseInt(prompt("El número es mayor. Inténtalo de nuevo:"));
     }
+} while (intento !== numeroSecreto)
 
-} while (intento !== adivinaNumero);
+alert("¡Felicidades! Adivinaste el número " + numeroSecreto); //imprimimos resultado de operación
