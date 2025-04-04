22a69d84f08b7e85b62cbe2183e75feb51999e16
lauuurars <1120098177@u.icesi.edu.co>
2025-04-04 00:15:52 -0500

    fin punto 9

---
+++ Act. 9 Casa/ciclos9.js
@@ -0,0 +1,9 @@
+//Desarrollo Actividad 9
+
+for (let i = 1; i <= 20; i++) {
+    console.log(i * 2);
+}
+
+//i va del 1 al 20, y se imprime su resultado multiplicado por 2
+
+//(i * 2) multiplicamos los 2 para obtener los números pares
